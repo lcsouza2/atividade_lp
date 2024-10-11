e35faba10d5449d45c5fb08abef8fb67c22b640c
lcsouza2 <150024147+lcsouza2@users.noreply.github.com>
2024-10-11 20:47:46 -0300

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Pessoa:
     """Atributos básicos de uma pessoa"""
     def __init__(self, nome, idade, genero):
@@ -15,7 +17,7 @@ class Professor(Pessoa):
     def __init__(self, nome, idade, genero, disciplina):
 
         super().__init__(nome=nome, idade=idade, genero=genero)
-
+        
         self.disciplina = disciplina
 
 
@@ -23,6 +25,7 @@ class Aluno(Pessoa):
     """Define um aluno"""
     def __init__(self, nome, idade, genero, matricula):
         super().__init__(nome, idade, genero)
+        
         self.matricula = matricula
 
 
@@ -30,4 +33,36 @@ class Administrador(Pessoa):
     """Define um administrador"""
     def __init__(self, nome, idade, genero, codigo):
         super().__init__(nome=nome, idade=idade, genero=genero)
+        
         self.codigo = codigo
+        
+class Database:
+    """Banco de dados kk"""
+    def __init__(self):
+        self.db = 'dados.db'
+
+
+    def connect(self):
+        """Conecta no banco e retorna a conexão e o cursor"""
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+        return conn, cur
+
+
+    def insert(self, table, data:dict):
+        """    
+        Insere dados no banco de dados
+        
+        Args:
+            data: dicionário no formato {coluna : valor}
+        """
+
+        colunas = ", ".join(i for i in data.keys())
+        
+        valores = tuple(data.values())
+        
+        conn, cur = self.connect()
+        
+        cur.execute(f"INSERT INTO {table}({colunas}) VALUES ({", ".join("?" for i in valores)});", valores)
+        
+        conn.commit()
